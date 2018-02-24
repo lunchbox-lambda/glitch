a8cd3550d4d865d643368aa7401c84be17a4c2cb
Zoltan Ujvary <ujvzolee@gmail.com>
2018-02-24 14:17:26 +0100

    Server uses compression

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const compression = require('compression')
 const app = express()
 
 const basePath = 'node_modules/@lunchbox/landing/dist'
 const publicPath = path.resolve(__dirname, basePath)
 
+app.use(compression())
 app.use(express.static(publicPath))
 
 app.use('/*', (req, res) => {
   res.sendFile(path.resolve(publicPath, 'index.html'))
 })
+
 
 const listener = app.listen(process.env.PORT, () => {
   console.log('Listening on port ' + listener.address().port)
