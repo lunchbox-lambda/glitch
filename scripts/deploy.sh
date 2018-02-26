d8c23500c878de800503234c836c70b0f33bca3c
Zoltan Ujvary <ujvzolee@gmail.com>
2018-02-26 21:53:48 +0100

    Deploy env variable tweak

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 curl -X POST -G \
 'https://api.glitch.com/project/githubImport' \
--d token=${GLITCH_TOKEN} \
+-d token=${GLITCH_IMPORT_TOKEN} \
 -d projectId=${GLITCH_PROJECT_ID} \
 -d repo=${GLITCH_REPO}
